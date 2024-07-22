11f5f684dde6ec751c62204ac0ae36eb90c174f5
Arhan Chaudhary <arhan.ch@gmail.com>
2024-07-22 00:00:13 -0700

    Simplify content script logic

--- contentScript.js
+++
@@ -1,6 +0,0 @@
-const s = document.createElement("script");
-s.src = chrome.runtime.getURL("arhan.js");
-s.onload = function () {
-  this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
